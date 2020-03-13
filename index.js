0996caa1822a56c17c2cc6f2f279f8cf19b70698
emily <mle4d@me.com>
2020-03-13 00:42:09 -0700

    starting on JS for each image's purpose

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector('.dancer').addEventListener('click', function() {
+  document.querySelector('.dancer').classList.toggle('about');
+  document.querySelector('.paint').classList.toggle('hidepaint');
+  document.querySelector('.mural').classList.toggle('hidemural');
+  document.querySelector('.actor').classList.toggle('hideactor');
+  window.body
+});
